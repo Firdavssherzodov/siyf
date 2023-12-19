c9876fe13b01ed2cb1654027f1764f218f4690c3
firdavs <frontendhtml1234@gmail.com>
2023-12-19 16:37:42 +0500

    a

--- src/views/Managiments/Managiment.vue
+++ src/views/Managiments/Managiment.vue
@@ -7,7 +7,7 @@
     class="card w-full xl:grid grid-cols-12 border md:gap-2 gap-5 lg:py-32 py-10"
   >
     <div
-      class="kard md:flex justify-center md:w-[700px] md:rounded-3xl md:h-[15rem] h-[20rem] md:col-span-9 col-span-1 md:my-2 xl:mx-80 sm:mx-auto  my-6"
+      class="kard md:flex justify-center md:w-[700px] w-[400px] md:rounded-3xl md:h-[15rem] h-[20rem] md:col-span-9 col-span-1 md:my-2 xl:mx-80 mx-auto  my-6"
       v-for="(item, index) of Managiments"
       :class="[
         index % 2 == 0
@@ -20,9 +20,9 @@
         alt="Image"
         width="400"
         preview
-        class="md:h-full h-[55%] sm:mr-3 mr-0 md:w-full w-full"
+        class="md:h-full h-[55%] sm:mr-3 mr-0"
       />
-      <div class="lg:w-[65%] w-[100%]">
+      <div class="md:w-[60%] w-[100%]">
         <p class="text-white text-center font-bold md:py-4 py-2 font-sans">
           {{ item.name }}
         </p>
@@ -99,6 +99,8 @@ const Managiments = [
   border-radius: 0px;
   border-top-right-radius: 0%;
   border-bottom-left-radius: 0%;
+  height: 100%;
+  width: -100%;
 }
 
 :deep button {
